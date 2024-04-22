9cae76914a9a2236bf33cbc4d916d1e2e097d753
Chris <goabonga@pm.me>
2024-04-22 12:33:33 +0200

    chore: Init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import goabongaLogo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://goabonga.com">
+          <img src={goabongaLogo} className="logo goabonga" alt="goabonga logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
